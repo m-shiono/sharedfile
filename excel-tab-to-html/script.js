b7e059c2eb15eddeb5ae1f56287b51be193d6c4b
m.shiono <89968009+m-shiono@users.noreply.github.com>
2023-05-02 15:25:19 +0900

    update file

--- excel-tab-to-html/script.js
+++ excel-tab-to-html/script.js
@@ -1,24 +1,88 @@
 function excelDataToHTML(input) {
     const rows = input.split('\n');
-    let html = '<table>';
+    let html = '<table>\n';
 
     for (const row of rows) {
-        html += '<tr>';
+        html += '  <tr>\n';
 
         const cells = row.split('\t');
         for (const cell of cells) {
-            html += `<td>${cell}</td>`;
+            html += `    <td>${cell}</td>\n`;
         }
 
-        html += '</tr>';
+        html += '  </tr>\n';
     }
 
     html += '</table>';
     return html;
 }
 
+function excelDataToMarkdown(input) {
+    const rows = input.split('\n');
+    let markdown = '';
+
+    let headerRow = true;
+    for (const row of rows) {
+        const cells = row.split('\t');
+        markdown += '| ' + cells.join(' | ') + ' |\n';
+
+        if (headerRow) {
+            markdown += '| ' + '--- |'.repeat(cells.length) + '\n';
+            headerRow = false;
+        }
+    }
+
+    return markdown;
+}
+
+function excelDataToBacklog(input) {
+    const rows = input.split('\n');
+    let backlog = '|';
+    let isHeaderRow = true;
+
+    for (const row of rows) {
+        const cells = row.split('\t');
+        if (isHeaderRow) {
+            backlog += cells.join('|h|') + '|h\n|';
+            isHeaderRow = false;
+        } else {
+            backlog += cells.join('|') + '|\n|';
+        }
+    }
+
+
+    // 最後の余分なパイプを削除
+    backlog = backlog.slice(0, -1);
+
+    return backlog;
+}
+
 document.getElementById('convert').addEventListener('click', () => {
     const excelData = document.getElementById('excel-data').value;
-    const html = excelDataToHTML(excelData);
-    document.getElementById('html-output').value = html;
+    const outputFormat = document.querySelector('input[name="output-format"]:checked').value;
+    let output;
+
+    if (outputFormat === 'html') {
+        output = excelDataToHTML(excelData);
+    } else if (outputFormat === 'markdown') {
+        output = excelDataToMarkdown(excelData);
+    } else {
+        output = excelDataToBacklog(excelData);
+    }
+
+    document.getElementById('output').value = output;
 });
+
+
+// ラジオボタンの表示制御
+document.querySelectorAll('input[name="output-format"]').forEach((radio) => {
+    radio.addEventListener('change', () => {
+        const backlogOptions = document.getElementById('backlog-options');
+        if (document.getElementById('backlog-format').checked) {
+            backlogOptions.style.display = 'block';
+        } else {
+            backlogOptions.style.display = 'none';
+        }
+    });
+});
+
